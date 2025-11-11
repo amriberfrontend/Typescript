c70e3255bbc7b271051bf74d7051b070cbe80235
Angel <amriberfrontend@gmail.com>
2025-11-11 13:30:42 +0100

    who gives a shit anymore

--- dia06/ejercicio-venta-coches.ts
+++ dia06/ejercicio-venta-coches.ts
@@ -4,14 +4,14 @@ interface IValorable {
 
 class Coche implements IValorable {
     id: string;
-    tipoRueda: Rueda;
+    ruedas: Rueda[];
     carroceria: Carroceria;
     volante: Volante;
     precio: number;
 
-    constructor(id: string, tipoRueda: Rueda, carroceria: Carroceria, volante: Volante, precio: number)  {
+    constructor(id: string, ruedas: Rueda[], carroceria: Carroceria, volante: Volante, precio: number)  {
         this.id = id;
-        this.tipoRueda = tipoRueda;
+        this.ruedas = ruedas;
         this.carroceria = carroceria;
         this.volante = volante;
         this.precio = precio
@@ -86,13 +86,16 @@ class Concesionario implements IValorable {
         return this.productos.reduce((total, p) => total + p.damePrecio(), 0);
     }
 
+    añadirCoche(coche: Coche): void {
+        this.productos.push(coche);
+    }
 }
 
 // create 4 ruedas
 const rueda1 = new Rueda("r1", 120, Terreno.CARRETERA);
-const rueda2 = new Rueda("r2", 150, Terreno.TODO_TERRENO);
+const rueda2 = new Rueda("r2", 150, Terreno.CARRETERA);
 const rueda3 = new Rueda("r3", 130, Terreno.CARRETERA);
-const rueda4 = new Rueda("r4", 140, Terreno.NIEVE);
+const rueda4 = new Rueda("r4", 140, Terreno.CARRETERA);
 
 // create 2 volante ruedas
 const volante1 = new Volante("v1", 250);
@@ -104,7 +107,13 @@ const carroceria2 = new Carroceria("c2", 2200, "azul");
 const carroceria3 = new Carroceria("c3", 2100, "negro");
 
 // create 1 car using the 4 ruedas, one carroceria and one volante rueda
-const coche1 = new Coche("coche1", rueda1, carroceria1, volante1, 15000);
+const coche1 = new Coche(
+    "coche1", 
+    [rueda1, rueda2, rueda3, rueda4], 
+    carroceria1, 
+    volante1, 
+    15000
+);
 
 // create dealership and add the items
 const concesionario = new Concesionario(
